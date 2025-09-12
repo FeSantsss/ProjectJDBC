99d17c63a1500a3c404ef4f249a364c58166cf2d
Felipy <tercetime18@gmail.com>
2025-09-12 12:25:12 -0300

    fix database properties and test connection

--- src/db/DB.java
+++ src/db/DB.java
@@ -8,28 +8,38 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
-	
+
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			}catch (SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 		return conn;
 	}
-	
+
+	public static void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}catch(IOException e) {
+		} catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
